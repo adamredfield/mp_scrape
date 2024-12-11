0503e4ed4769651e36dfb68b8cb49e9400fc917d
adamredfield <redfield.adam.r@gmail.com>
2024-12-11 00:41:49 -0700

    removed try block to try to better observe

--- src/scraping/helper_functions.py
+++ src/scraping/helper_functions.py
@@ -361,125 +361,120 @@ def process_page(page_number, ticks_url, user_id, retry_count=0):
                 print(f"Found {len(tick_rows)} rows to process")
 
                 for row in tick_rows:
-                    try:
-                        tick_details = row.find('td', class_='text-warm small pt-0')
-                        print(f"Found tick details: {tick_details}")
-                    
-                        if not tick_details:
-                            cells = row.find_all('td')
-                            route_name = ' '.join(cells[0].text.strip().replace('●', '').split())
-                            print(f'Retrieving data for {route_name}')
-                            route_link = row.find('a', href=True)['href']
-                            route_id = route_link.split('/route/')[1].split('/')[0]
-                            route_exists = queries.check_route_exists(cursor, route_id)
-                            if route_exists:
-                                print(f"Route {route_name} already exists in the database.")
-                                current_route_data = {
+                    tick_details = row.find('td', class_='text-warm small pt-0')
+                    print(f"Found tick details: {tick_details}")
+                
+                    if not tick_details:
+                        cells = row.find_all('td')
+                        route_name = ' '.join(cells[0].text.strip().replace('●', '').split())
+                        print(f'Retrieving data for {route_name}')
+                        route_link = row.find('a', href=True)['href']
+                        route_id = route_link.split('/route/')[1].split('/')[0]
+                        route_exists = queries.check_route_exists(cursor, route_id)
+                        if route_exists:
+                            print(f"Route {route_name} already exists in the database.")
+                            current_route_data = {
+                            'route_id': route_id,
+                            'route_name': route_name
+                        }
+                        else:
+                            route_html_content = fetch_dynamic_page_content(page, route_link)
+                            route_soup = BeautifulSoup(route_html_content, 'html.parser')
+
+                            route_attributes = get_route_attributes(route_soup)
+                            route_location = parse_location(route_attributes.get('formatted_location'))
+                            route_sections = get_route_sections(route_soup)
+                            route_details = get_route_details(route_soup)
+                            comments = get_comments(route_soup)
+
+                            current_route_data = {
+                                'route_id': route_id,
+                                'route_name': route_name,
+                                'route_url': route_link,
+                                'yds_rating': route_attributes.get('yds_rating'),  
+                                'hueco_rating': route_attributes.get('hueco_rating'),
+                                'aid_rating': route_attributes.get('aid_rating'),
+                                'danger_rating': route_attributes.get('danger_rating'),
+                                'avg_stars': route_attributes.get('avg_stars'),
+                                'num_votes': route_attributes.get('num_ratings'),
+                                'region': route_location.get('region'),
+                                'main_area': route_location.get('main_area'),
+                                'sub_area': route_location.get('sub_area'),
+                                'specific_location': route_location.get('specific_location'),
+                                'route_type': route_details.get('route_type'),
+                                'length_ft': route_details.get('length_ft'),
+                                'pitches': route_details.get('pitches'),
+                                'commitment_grade': route_details.get('commitment_grade'),
+                                'fa': route_details.get('fa'),
+                                'description': route_sections.get('description'),
+                                'protection': route_sections.get('protection'),
+                                'primary_photo_url': route_attributes.get('primary_photo_url'),
+                                'insert_date': datetime.now(timezone.utc).isoformat()
+                            }
+
+                            comments_dict = [{'route_id': route_id, 'comment': comment, 'insert_date': datetime.now(timezone.utc).isoformat()} for comment in comments]
+
+                            queries.insert_route(cursor, conn, current_route_data)
+                            queries.insert_comments(cursor, conn, comments_dict)
+                            conn.commit()
+
+                            current_route_data = {
                                 'route_id': route_id,
                                 'route_name': route_name
                             }
-                            else:
-                                route_html_content = fetch_dynamic_page_content(page, route_link)
-                                route_soup = BeautifulSoup(route_html_content, 'html.parser')
+                        
+                        # Check if tick details row rather than a route row. 
+                        if tick_details and current_route_data:
+                            # Append the additional info to the previous row's data
+                            tick_details_text = tick_details.text.strip()
 
-                                route_attributes = get_route_attributes(route_soup)
-                                route_location = parse_location(route_attributes.get('formatted_location'))
-                                route_sections = get_route_sections(route_soup)
-                                route_details = get_route_details(route_soup)
-                                comments = get_comments(route_soup)
+                            date_pattern = r'[A-Z][a-z]{2}\s+\d{1,2},\s+\d{4}'
+                            date_match = re.search(date_pattern, tick_details_text)
+                            tick_date = date_match.group() if date_match else None
 
-                                current_route_data = {
-                                    'route_id': route_id,
-                                    'route_name': route_name,
-                                    'route_url': route_link,
-                                    'yds_rating': route_attributes.get('yds_rating'),  
-                                    'hueco_rating': route_attributes.get('hueco_rating'),
-                                    'aid_rating': route_attributes.get('aid_rating'),
-                                    'danger_rating': route_attributes.get('danger_rating'),
-                                    'avg_stars': route_attributes.get('avg_stars'),
-                                    'num_votes': route_attributes.get('num_ratings'),
-                                    'region': route_location.get('region'),
-                                    'main_area': route_location.get('main_area'),
-                                    'sub_area': route_location.get('sub_area'),
-                                    'specific_location': route_location.get('specific_location'),
-                                    'route_type': route_details.get('route_type'),
-                                    'length_ft': route_details.get('length_ft'),
-                                    'pitches': route_details.get('pitches'),
-                                    'commitment_grade': route_details.get('commitment_grade'),
-                                    'fa': route_details.get('fa'),
-                                    'description': route_sections.get('description'),
-                                    'protection': route_sections.get('protection'),
-                                    'primary_photo_url': route_attributes.get('primary_photo_url'),
-                                    'insert_date': datetime.now(timezone.utc).isoformat()
-                                }
+                            tick_type = None
+                            tick_note = None
 
-                                comments_dict = [{'route_id': route_id, 'comment': comment, 'insert_date': datetime.now(timezone.utc).isoformat()} for comment in comments]
+                            valid_tick_types = [
+                                'Solo', 'TR', 'Follow', 'Lead',
+                                'Lead / Onsight', 'Lead / Flash',
+                                'Lead / Redpoint', 'Lead / Pinkpoint',
+                                'Lead / Fell/Hung'
+                            ]
 
-                                queries.insert_route(cursor, conn, current_route_data)
-                                queries.insert_comments(cursor, conn, comments_dict)
-                                conn.commit()
-
-                                current_route_data = {
-                                    'route_id': route_id,
-                                    'route_name': route_name
-                                }
-                            
-                            # Check if tick details row rather than a route row. 
-                            if tick_details and current_route_data:
-                                # Append the additional info to the previous row's data
-                                tick_details_text = tick_details.text.strip()
-
-                                date_pattern = r'[A-Z][a-z]{2}\s+\d{1,2},\s+\d{4}'
-                                date_match = re.search(date_pattern, tick_details_text)
-                                tick_date = date_match.group() if date_match else None
-
-                                tick_type = None
-                                tick_note = None
-
-                                valid_tick_types = [
-                                    'Solo', 'TR', 'Follow', 'Lead',
-                                    'Lead / Onsight', 'Lead / Flash',
-                                    'Lead / Redpoint', 'Lead / Pinkpoint',
-                                    'Lead / Fell/Hung'
-                                ]
-
-                                if ' · ' in tick_details_text:
-                                    post_date_text = tick_details_text.split(' · ')[1]  # Get everything after the bullet, following date
-                                    if '.' in post_date_text:
-                                        parts = post_date_text.split('.', 1)
-                                        if "pitches" in parts[0].lower():
-                                            next_parts = parts[1].split('.', 1)
-                                            potential_type = next_parts[0].strip()
-                                            if potential_type in valid_tick_types:
-                                                tick_type = potential_type
-                                                tick_note = next_parts[1].strip() if len(next_parts) > 1 else None
-                                            else:
-                                                tick_note = parts[1].strip()
+                            if ' · ' in tick_details_text:
+                                post_date_text = tick_details_text.split(' · ')[1]  # Get everything after the bullet, following date
+                                if '.' in post_date_text:
+                                    parts = post_date_text.split('.', 1)
+                                    if "pitches" in parts[0].lower():
+                                        next_parts = parts[1].split('.', 1)
+                                        potential_type = next_parts[0].strip()
+                                        if potential_type in valid_tick_types:
+                                            tick_type = potential_type
+                                            tick_note = next_parts[1].strip() if len(next_parts) > 1 else None
                                         else:
-                                            potential_type = parts[0].strip()
-                                            if potential_type in valid_tick_types:
-                                                tick_type = potential_type
-                                                tick_note = parts[1].strip() if len(parts) > 1 else None
-                                            else:
-                                                tick_note = parts[1].strip()
+                                            tick_note = parts[1].strip()
                                     else:
-                                        tick_note = post_date_text.strip()
-                                        
-                                tick_data = {
-                                    'user_id': user_id,
-                                    'route_id': current_route_data['route_id'],
-                                    'date': tick_date,
-                                    'type': tick_type,
-                                    'note': tick_note,
-                                    'insert_date': datetime.now(timezone.utc).isoformat()
-                                }
-                                queries.insert_tick(cursor, conn, tick_data)
-                                conn.commit()
-                                current_route_data = None
-
-                    except Exception as e:
-                        print(f"Error processing row: {str(e)}")
-                        continue  
+                                        potential_type = parts[0].strip()
+                                        if potential_type in valid_tick_types:
+                                            tick_type = potential_type
+                                            tick_note = parts[1].strip() if len(parts) > 1 else None
+                                        else:
+                                            tick_note = parts[1].strip()
+                                else:
+                                    tick_note = post_date_text.strip()
+                                    
+                            tick_data = {
+                                'user_id': user_id,
+                                'route_id': current_route_data['route_id'],
+                                'date': tick_date,
+                                'type': tick_type,
+                                'note': tick_note,
+                                'insert_date': datetime.now(timezone.utc).isoformat()
+                            }
+                            queries.insert_tick(cursor, conn, tick_data)
+                            conn.commit()
+                            current_route_data = None
                 
                 print(f'Successfully processed page {page_number}')
             
