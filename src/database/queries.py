9f8646b16c97c4bec609a88121b7e552cf032d67
adamredfield <redfield.adam.r@gmail.com>
2024-12-11 16:25:53 -0700

    changed database connection structure and further modularized process_pages.

--- src/database/queries.py
+++ src/database/queries.py
@@ -1,66 +1,89 @@
-def insert_route(cursor, connection, route_data):
-    route_sql = '''
-    INSERT INTO routes.Routes (
-        id, route_name, route_url, yds_rating, hueco_rating, aid_rating, danger_rating, 
-        avg_stars, num_votes, region, main_area, sub_area, specific_location, route_type, 
-        length_ft, pitches, commitment_grade, fa, description, protection, primary_photo_url, 
-        insert_date
-    ) VALUES (     
-        %(route_id)s, %(route_name)s, %(route_url)s, %(yds_rating)s, %(hueco_rating)s, 
-        %(aid_rating)s, %(danger_rating)s, %(avg_stars)s, %(num_votes)s, %(region)s, 
-        %(main_area)s, %(sub_area)s, %(specific_location)s, %(route_type)s, %(length_ft)s, 
-        %(pitches)s, %(commitment_grade)s, %(fa)s, %(description)s, %(protection)s, 
-        %(primary_photo_url)s, %(insert_date)s
-    )
-    ON CONFLICT (id) DO NOTHING
-    '''
+def insert_comments_batch(cursor, comments):  
     try:
-        cursor.execute(route_sql, route_data)
-        connection.commit()
-        print(f"inserted route {route_data['route_name']}")
-    except Exception as e:
-        print(f"Error inserting {route_data['route_name']}: {e}")
-        connection.rollback()
+        args_str = ','.join(
+            cursor.mogrify(
+                "(%(route_id)s, %(comment)s, encode(digest(%(comment)s, 'sha256'), 'hex'), %(insert_date)s)",
+                comment
+            ).decode('utf-8')
+            for comment in comments
+        )
 
-def insert_comments(cursor, connection, comments):
-    if comments:
-        comments_sql = '''
+        comments_sql = f"""
         INSERT INTO routes.RouteComments (
             route_id, comment, comment_hash, insert_date
-        ) VALUES (
-            %(route_id)s, %(comment)s, encode(digest(%(comment)s, 'sha256'), 'hex'), %(insert_date)s
-        )
+        ) VALUES {args_str}
         ON CONFLICT (route_id, comment_hash) DO NOTHING
-        '''
-        try:    
-            cursor.executemany(comments_sql, comments)
-            print(f"inserted {len(comments)} comments for route {comments[0]['route_id']}")
-            connection.commit()
-        except Exception as e:
-            print(f"Error inserting comments: {e}")
-            connection.rollback()
-
-def insert_tick(cursor, connection, tick_data):
-    tick_sql = '''
-    INSERT INTO routes.Ticks (
-        user_id, route_id, date, type, note, insert_date
-    ) VALUES (
-        %(user_id)s, %(route_id)s, %(date)s, %(type)s, %(note)s, %(insert_date)s
-    )
-    ON CONFLICT (user_id, route_id, date) DO NOTHING
-    '''
-    try:
-        cursor.execute(tick_sql, tick_data)
-        connection.commit()
-        print(f"inserted tick on {tick_data['date']} for route {tick_data['route_id']}")
+        """
+        
+        cursor.execute(comments_sql)
+        print(f"Successfully inserted {len(comments)} comments")
+        
     except Exception as e:
-        print(f"Error inserting tick: {e}")
-        connection.rollback()
+        print(f"Error inserting comments batch: {str(e)}")
+        raise
 
-def check_route_exists(cursor, route_id):
-    """Check if route exists in PostgreSQL"""
-    cursor.execute(
-        "SELECT EXISTS(SELECT 1 FROM routes.Routes WHERE id = %s)",
-        (route_id,)
+def insert_ticks_batch(cursor, tick_data):
+    args_str = ','.join(
+        cursor.mogrify(
+            "(%(user_id)s, %(route_id)s, %(date)s, %(type)s, %(note)s, %(insert_date)s)",
+            tick
+        ).decode('utf-8') 
+        for tick in tick_data
     )
-    return cursor.fetchone()[0]   
+
+    tick_sql = f"""
+        INSERT INTO routes.Ticks (user_id, route_id, date, type, note, insert_date)
+        VALUES {args_str}
+        ON CONFLICT (user_id, route_id, date) DO NOTHING
+    """
+    try:
+        cursor.execute(tick_sql)
+        print(f"Successfully inserted {len(tick_data)} ticks")
+    except Exception as e:
+        print(f"Error inserting tick batch: {str(e)}")
+        raise
+
+def check_routes_exists(cursor, route_ids):
+    """Check if routes exist in database"""
+
+    route_id_list = ','.join(['%s'] * len(route_ids))
+    cursor.execute(
+        f"SELECT id FROM routes.Routes WHERE id IN ({route_id_list})",
+        tuple(route_ids)
+    )
+    return {row[0] for row in cursor.fetchall()} # return set of route ids that exist
+
+def insert_routes_batch(cursor, routes_data):
+    try:
+        args_str = ','.join(
+            cursor.mogrify(
+                """(
+                    %(route_id)s, %(route_name)s, %(route_url)s, %(yds_rating)s, 
+                    %(hueco_rating)s, %(aid_rating)s, %(danger_rating)s, %(avg_stars)s, 
+                    %(num_votes)s, %(region)s, %(main_area)s, %(sub_area)s, 
+                    %(specific_location)s, %(route_type)s, %(length_ft)s, %(pitches)s, 
+                    %(commitment_grade)s, %(fa)s, %(description)s, %(protection)s, 
+                    %(primary_photo_url)s, %(insert_date)s
+                )""",
+                route
+            ).decode('utf-8')
+            for route in routes_data
+        )
+
+        route_sql = f"""
+        INSERT INTO routes.Routes (
+            id, route_name, route_url, yds_rating, hueco_rating, aid_rating, 
+            danger_rating, avg_stars, num_votes, region, main_area, sub_area, 
+            specific_location, route_type, length_ft, pitches, commitment_grade, 
+            fa, description, protection, primary_photo_url, insert_date
+        ) VALUES {args_str}
+        ON CONFLICT (id) DO NOTHING
+        """
+        
+        cursor.execute(route_sql)
+        print(f"Successfully inserted {len(routes_data)} routes")
+        
+    except Exception as e:
+        print(f"Error inserting routes batch: {str(e)}")
+        raise
+
